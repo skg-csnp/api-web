b55ab51f359a3b14c229acdb9f6135e9f2f320d8
ToanNV (home) <nvt87x@gmail.com>
2025-07-29 20:51:00 +0700

    Implement domain events for SaveChangesAsync

--- src/Credential/Csnp.Credential.Infrastructure/Persistence/CredentialDbContext.cs
+++ src/Credential/Csnp.Credential.Infrastructure/Persistence/CredentialDbContext.cs
@@ -1,3 +1,6 @@
+using Csnp.Credential.Application.Events;
+using Csnp.Credential.Infrastructure.Events;
+using Csnp.SeedWork.Domain.Events;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +17,46 @@ public class CredentialDbContext : IdentityDbContext<UserEntity, RoleEntity, lon
         builder.ApplyConfigurationsFromAssembly(typeof(UserEntity).Assembly);
     }
 
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        int result = await base.SaveChangesAsync(cancellationToken);
+
+        var domainEntities = ChangeTracker
+            .Entries<IEntityWithEvents>()
+            .Where(x => x.Entity.DomainEvents.Any())
+            .Select(x => x.Entity)
+            .ToList();
+
+        var domainEvents = domainEntities
+            .SelectMany(x => x.DomainEvents)
+            .ToList();
+
+        await _dispatcher.DispatchAsync(domainEvents, cancellationToken);
+
+        foreach (IEntityWithEvents? entity in domainEntities)
+        {
+            entity.ClearDomainEvents();
+        }
+
+        return result;
+    }
+
     #endregion
 
     #region -- Methods --
 
     public CredentialDbContext(DbContextOptions<CredentialDbContext> options) : base(options) { }
+
+    public CredentialDbContext(DbContextOptions<CredentialDbContext> options, IDomainEventDispatcher dispatcher) : base(options)
+    {
+        _dispatcher = dispatcher;
+    }
+
+    #endregion
+
+    #region -- Fields --
+
+    private readonly IDomainEventDispatcher _dispatcher = new NoOpDomainEventDispatcher();
 
     #endregion
 }
